3f9adc96b2163fc94fd476904eba87fea59b8083
F14T7 <fsh07@outlook.de>
2022-11-27 20:34:15 +0100

    ad

--- open.js
+++ open.js
@@ -1,6 +1,14 @@
-var element = document.querySelector(".door");
-element.addEventListener("click", toggleDoor);
 
-function toggleDoor() {
-  element.classList.toggle("doorOpen");
+const elements = document.querySelectorAll('.door');
+
+elements[0].addEventListener('click', function onClick() {
+  console.log('box clicked');
+});
+
+for (const element of elements) {
+  element.addEventListener('click', function onClick() {
+    console.log('lol');
+    element.classList.toggle("doorOpen");
+  })
 }
+
