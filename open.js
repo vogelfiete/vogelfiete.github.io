649d691db325f5356b7920108ec5a00988517569
F14T7 <fsh07@outlook.de>
2022-11-30 15:41:11 +0100

    q

--- open.js
+++ open.js
@@ -1,14 +1,61 @@
 
 const elements = document.querySelectorAll('.door');
+const elementium = document.querySelectorAll('.backDoor')
 
 elements[0].addEventListener('click', function onClick() {
   console.log('box clicked');
 });
 
-for (const element of elements) {
-  element.addEventListener('click', function onClick() {
-    console.log('lol');
-    element.classList.toggle("doorOpen");
-  })
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+function abc(){ 
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+  console.log('lolll');
 }
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+
+
+    for (const element of elements) {
+  
+      element.addEventListener('click', function efg() {
+        console.log('lol');
+
+        if (element.className === "door") {
+          element.classList.toggle("doorOpen");
+          }
+
+        else if (element.className != "door") { 
+          open();
+          };
+
+    })
+  }
+  function open() {
+  for (var i=0; i<= 23; ++i) {
+    elementium[i].onClick = abc();
+    console.log(i)
+    } 
+  }
+
+  
